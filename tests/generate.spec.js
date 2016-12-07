1d1f3feb5c010732ae2af797e2689278028b85cb
Morlene Fisher <morlene@ashleylawrence.com>
2016-12-07 09:15:29 +0000

    Added test for double linked refs

--- tests/generate.spec.js
+++ tests/generate.spec.js
@@ -2,20 +2,28 @@
 let fs = require('fs');
 let handler = require(process.cwd() + '/index.js');
 
-describe('Test generate schema', function() {
+describe('Test generate schema', function () {
 
-    it('has base exporters', function() {
+    it('has base exporters', function () {
         expect(handler).toBeDefined();
     });
 
-    it('can generate stuff', function(done) {
+    it('can generate stuff', function (done) {
         let dest = process.cwd() + '/examples/results.json';
-        let source = process.cwd() + '/examples/uuid.json';
+        let source = process.cwd() + '/examples/basic.json';
         handler(source, dest);
-         fs.stat(dest, function(err, stats){
-           expect(stats).toBeDefined();
-           done();
+        fs.stat(dest, function (err, stats) {
+            expect(stats).toBeDefined();
+            done();
 
-         });
+        });
+    })
+
+    it('makes definitions', function () {
+        let file = process.cwd() + '/examples/results.json';
+        let o = JSON.parse(fs.readFileSync(file));
+        expect(o['$schema']).toBeDefined();
+        expect(o.properties.uuid).toBeDefined();
+        expect(o.properties.another_uuid).toBeDefined();
     })
 })
